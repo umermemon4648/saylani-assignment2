58fcd4c15136044f1ab84511b00dcb157d27ac0c
umermemon4648 <umermemon4648@gmail.com>
2023-04-21 01:57:06 +0500

    solved Q2

--- app.js
+++ app.js
@@ -15,3 +15,29 @@ function addWithOffset(x){
 
 const functionInput = addWithOffset(5)
 console.log(functionInput(6))
+
+
+/*
+Q2: Write a recursive function that searches an array for a specific value. The
+function should return true if the value is found, and false if it is not. You
+can assume that the array is not nested.
+*/
+
+//Here is the Solution of Q2
+
+function searchArrayValue(arr, key, index = 0){
+    if(index>=arr.length){
+        return false
+    }
+    else if(arr[index] == key){
+        return true
+    }
+    else{
+        return searchArrayValue(arr, key, index+1)
+    }
+}
+
+const arr = [1,3,5,7,2,4,8,9]
+const key = 4
+
+console.log(searchArrayValue(arr, key))
