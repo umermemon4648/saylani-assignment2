17b65c1ee006850df24fd3f09bb84c87be77514e
umermemon4648 <umermemon4648@gmail.com>
2023-04-23 01:42:06 +0500

    Solved Q3

--- app.js
+++ app.js
@@ -41,3 +41,20 @@ const arr = [1,3,5,7,2,4,8,9]
 const key = 4
 
 console.log(searchArrayValue(arr, key))
+
+/*
+Q3: Write a function that adds a new paragraph element to the bottom of an
+HTML document. The function should take a string argument that will be
+used as the text content of the new paragraph element.
+
+*/
+
+//Here is the Solution of Q3
+function addingParagraph(paraText){
+
+    let tag = document.createElement('p')
+    tag.innerHTML = paraText
+    document.body.appendChild(tag)
+
+}
+addingParagraph("Hello! Fellow Developer")
